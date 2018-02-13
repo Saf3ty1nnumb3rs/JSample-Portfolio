198f5bb3f4d12918addc2b2687f92c3550e43aad
Josh Sample <joshuawsample@gmail.com>
2018-02-13 02:27:46 -0500

    Footer Created w/ contact no links

--- js/app.js
+++ js/app.js
@@ -1,9 +1,49 @@
+const DisplayCarrier = {
+
+    topFade: function(){
 
 
+var header = $('.top-section, .bottom-logo');
+var range = 200;
+
+$(window).on('scroll', () => {
+  
+  var scrollTop = $(this).scrollTop(),
+      height = header.outerHeight(),
+      offset = height / 2,
+      calc = 1 - (scrollTop - offset + range) / range;
+        header.css({ 'opacity': calc });
+
+    if (calc > '1') {
+        header.css({ 'opacity': 1 });
+    } else if ( calc < '0' ) {
+        header.css({ 'opacity': 0 });
+    }
+  
+});
+    },
+
+    navFill: function() {
+        var scrollStart = 0;
+        var startChange = $('#start-change')
+        var offset = startChange.offset()
+        $(document).scroll(() => {
+            scrollStart = $(this).scrollTop();
+            console.log(scrollStart)
+            console.log(offset.top)
+            if(scrollStart > offset.top) {
+                $('#nav-fill>div').css({'background-color': 'rgba(126, 126, 126, 0.9)', 'z-index':'900', 'transition':'2s'});
+            } else {
+               $('#nav-fill>div').css('background-color', 'transparent');
+            } 
+        })
+
+    },
 
 
+}
 
-const clickController = {
+const ClickController = {
 // Need to set if sttement to check for has class 'is-active' then do below if false
 
 
@@ -38,7 +78,8 @@ const clickController = {
 
 
 window.onload = function () {
-    $('.hamburger').on('click', clickController.hamburgerHelper);
-
+    $('.hamburger').on('click', ClickController.hamburgerHelper);
+    $(window).on('scroll', DisplayCarrier.topFade);
+    $(window).on('scroll', DisplayCarrier.navFill);
 
 }
